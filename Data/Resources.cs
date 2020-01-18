33b1bd5f8667f9a6deba2101cbc77c3cdddaf23b
Yavor Rusev <135iq.com@gmail.com>
2020-01-18 13:54:45 +0200

    Refactor, Refactor, Refactor

--- Data/Resources.cs
+++ Data/Resources.cs
@@ -1,12 +1,15 @@
 ﻿
+using System.Reflection;
+
 namespace FineDustParticlesSensorData
 {
     static class Resources
     {
+        //This URI is fixed due to task constraints.
         private static readonly string Uri = "https://data.sensor.community/airrohr/v1/sensor/10001/";
 
-        private static readonly string path = @"..\..\..\DustSensorDataLog.txt";
-        // static string path =  Assembly.GetExecutingAssembly().Location + "Log.txt";
+       // private static readonly string path = @"..\..\..\DustSensorDataLog.txt";
+         static string path =  Assembly.GetExecutingAssembly().Location + "Log.txt";
 
         public static string GetURI()
         {
